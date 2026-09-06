030b9b6786cfed957e7f10e1df2ce5c137ad4667
adrikocyrus <adrikocyurs48@gmail.com>
2026-09-06 09:48:58 +0300

    EXERCISE 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,21 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        // REQUIREMENT 1: Generate random number from 1 to 100
         Random randomGenerator = new Random();
         int magicNumber = randomGenerator.Next(1, 101);
         
         int guess = -1;
         
-        // REQUIREMENT 2: Loop until user guesses correctly
         while (guess != magicNumber)
         {
-            // Ask user for a guess
             Console.Write("What is your guess? ");
             string guessInput = Console.ReadLine();
             guess = int.Parse(guessInput);
             
-            // REQUIREMENT 3: If statement for higher/lower/correct
             if (guess < magicNumber)
             {
                 Console.WriteLine("Higher");
